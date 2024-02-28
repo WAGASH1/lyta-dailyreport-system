f4d1a4fb105ae2400fd6c054c53cb6c5ae1768ed
WAGASH1 <takuannshoui@icloud.com>
2024-02-29 07:16:16 +0900

    lesson34 途中

---
+++ src/main/java/com/techacademy/entity/Report.java
@@ -0,0 +1,72 @@
+
+package com.techacademy.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.SQLRestriction;
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "reports")
+@SQLRestriction("delete_flg = false")
+public class Report {
+
+
+
+    // ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotEmpty
+    private int id;
+
+    // 日付
+    @NotEmpty
+    private Date reportDate;
+
+
+    // タイトル
+    @Column(length = 100, nullable = false)
+    @NotEmpty
+    @Length(max = 100)
+    private String title;
+
+
+    // 内容
+    @NotEmpty
+    private String content;
+
+
+    // 社員番号
+    @ManyToOne
+    @JoinColumn(name="employee_code", referencedColumnName="code" , nullable = false)
+    private Employee employee;
+
+
+    // 削除フラグ(論理削除を行うため)
+    @Column(columnDefinition="TINYINT", nullable = false)
+    private boolean deleteFlg;
+
+    // 登録日時
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    // 更新日時
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
